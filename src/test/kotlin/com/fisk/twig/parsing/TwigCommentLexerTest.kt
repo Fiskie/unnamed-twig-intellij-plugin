d1d5a380944672eb61fabb9329841bbedbd5afb6
fisk <myself@fiskie.me>
2018-01-07 11:15:27 +0000

    rm trailing whites

---
+++ src/test/kotlin/com/fisk/twig/parsing/TwigCommentLexerTest.kt
@@ -0,0 +1,40 @@
+package com.fisk.twig.parsing
+
+import com.fisk.twig.parsing.TwigTokenTypes.COMMENT_CLOSE
+import com.fisk.twig.parsing.TwigTokenTypes.COMMENT_CONTENT
+import com.fisk.twig.parsing.TwigTokenTypes.COMMENT_OPEN
+import com.fisk.twig.parsing.TwigTokenTypes.UNCLOSED_COMMENT
+
+class TwigCommentLexerTest : TwigLexerTest() {
+    fun testComment() {
+        // unlike with statements/comments, we have no reason to tokenize whitespace
+        val result = tokenize("{# foo #}")
+        result.shouldMatchTokenTypes(COMMENT_OPEN, COMMENT_CONTENT, COMMENT_CLOSE)
+        result.shouldMatchTokenContent("{#", " foo ", "#}")
+    }
+
+    fun testCommentWithWhitespaceControl() {
+        // again, unlike with statements/comments, we have no reason to tokenize whitespace
+        val result = tokenize("{#- foo -#}")
+        result.shouldMatchTokenTypes(COMMENT_OPEN, COMMENT_CONTENT, COMMENT_CLOSE)
+        result.shouldMatchTokenContent("{#-", " foo ", "-#}")
+    }
+
+    fun testUnformattedCommentWithWhitespaceControl() {
+        val result = tokenize("{#-foo-#}")
+        result.shouldMatchTokenTypes(COMMENT_OPEN, COMMENT_CONTENT, COMMENT_CLOSE)
+        result.shouldMatchTokenContent("{#-", "foo", "-#}")
+    }
+
+    fun testUnformattedComment() {
+        val result = tokenize("{#foo#}")
+        result.shouldMatchTokenTypes(COMMENT_OPEN, COMMENT_CONTENT, COMMENT_CLOSE)
+        result.shouldMatchTokenContent("{#", "foo", "#}")
+    }
+
+    fun testUnclosedComment() {
+        val result = tokenize("{#foo")
+        result.shouldMatchTokenTypes(COMMENT_OPEN, UNCLOSED_COMMENT)
+        result.shouldMatchTokenContent("{#", "foo")
+    }
+}
---
+++ src/test/kotlin/com/fisk/twig/parsing/TwigContentLexerTest.kt
@@ -0,0 +1,20 @@
+package com.fisk.twig.parsing
+
+import com.fisk.twig.parsing.TwigTokenTypes.CONTENT
+
+class TwigContentLexerTest : TwigLexerTest() {
+    fun testContent() {
+        val result = tokenize("<div>etc</div>")
+        result.shouldMatchTokenTypes(CONTENT)
+        result.shouldMatchTokenContent("<div>etc</div>")
+    }
+
+    fun testJavaScriptContent() {
+        // This function will fail until content lexing consumes { properly
+        // Functionally this probably won't change anything, but
+        // I'd like it if it weren't so hacky
+        val result = tokenize("var foo = function() { console.log(bar) }")
+        result.shouldMatchTokenTypes(CONTENT)
+        result.shouldMatchTokenContent("var foo = function() { console.log(bar) }")
+    }
+}
